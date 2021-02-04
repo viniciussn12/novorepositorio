5cba6136c52d234e0ede205c3d3f07c532d06082
viniciussn12 <56917996+viniciussn12@users.noreply.github.com>
2021-02-04 00:04:58 -0300

    Criação de um arquivo de teste

---
+++ index.php
@@ -0,0 +1,23 @@
+<DOCTYPE html>
+<html>
+  <head>
+    <title>Arquivo Teste</title>
+  </head>
+  <body>
+    <h1>
+      <?php
+        echo "1 2 3 testando";
+      ?>
+    </h1>
+    <h2>
+      <?php
+        echo "1 2 3 test";
+      ?>
+    </h2>
+    <h3>
+      <?php
+        echo "1 2 3";
+      ?>
+    </h3>
+  </body>
+</html>
